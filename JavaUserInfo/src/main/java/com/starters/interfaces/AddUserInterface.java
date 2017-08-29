30e474839f753bc5aef5ebf890e3c4b8bd8527fb
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-08-28 22:38:40 -0400

    removing unnecessary file uploads

--- JavaUserInfo/src/main/java/com/starters/interfaces/AddUserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.starters.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.starters.model.User;
-
-public interface AddUserInterface extends CrudRepository<User,Integer>{
-
-}
