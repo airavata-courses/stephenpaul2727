bdf31bfa0ce74df092808316ed40790cda36cb12
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-09-11 01:25:56 -0400

    Added DockerFiles for the Microservices Still need to compose docker services with rabbitmq to run them perfectly

--- Assignment2/JavaUserInfo/src/main/java/com/starters/Send.java
+++ Assignment2/JavaUserInfo/src/main/java/com/starters/Send.java
@@ -16,7 +16,7 @@ public class Send implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend("java-exchange","python-queue", "Hello from RabbitMQ");
+//        rabbitTemplate.convertAndSend("java-exchange","python-queue", "Hello from RabbitMQ");
 	}
 }
 
