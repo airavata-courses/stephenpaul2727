187a2a34304c5d2edc0168fa6ca4e5ae141e5b22
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-09-28 18:30:20 -0400

    timeteller consumes on build

--- Assignment2/timeteller/app/Http/Controllers/HomeController.php
+++ Assignment2/timeteller/app/Http/Controllers/HomeController.php
@@ -13,6 +13,46 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class HomeController extends Controller
 {
+    /************************************************/
+
+    $connection = new AMQPStreamConnection('129.114.104.44', 5672, 'guest', 'guest');
+    $channel = $connection->channel();
+    $channel->exchange_declare('java-exchange', 'topic', false, true, false);
+    $channel->queue_declare("php-queue", false, true, false, false);
+    $channel->queue_bind("php-queue", 'java-exchange', 'php-queue');
+    $callback = function($msg){
+        echo 'RoutingKEY:',$msg->delivery_info['routing_key'], ': Requesting: ', $msg->body, "\n";
+        if($msg->body == "time"){
+            $mytime = Carbon::now();
+            $finalTime = $mytime->toDateTimeString();
+            echo $finalTime;
+            $connection = new AMQPStreamConnection('129.114.104.44', 5672, 'guest', 'guest');
+            $channel = $connection->channel();
+            $channel->exchange_declare("java-exchange","topic",false,true,false);
+            $routingKey = "api-queue";
+            $msg = new AMQPMessage($finalTime,array('delivery_mode' => 2));
+            $channel->basic_publish($msg,"java-exchange",$routingKey);
+
+        }
+        else if($msg->body == "userinfo"){
+            $result = file_get_contents("http://129.114.104.44:8090/getuserdata");
+            $connection = new AMQPStreamConnection('129.114.104.44', 5672, 'guest', 'guest');
+            $channel = $connection->channel();
+            $channel->exchange_declare("java-exchange","topic",false,true,false);
+            $routingKey = "api-queue";
+            $msg = new AMQPMessage($result,array('delivery_mode' => 2));
+            $channel->basic_publish($msg,"java-exchange",$routingKey);
+
+        }
+        else {}
+    };
+    $channel->basic_consume("php-queue", '', false, true, false, false, $callback);
+    while(count($channel->callbacks)) {
+        $channel->wait();
+    }
+
+    /**************************************************/
+
 
     /**
      * Show the application dashboard.
